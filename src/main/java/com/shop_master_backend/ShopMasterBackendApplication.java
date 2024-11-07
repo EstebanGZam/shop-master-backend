74766177ba29963d70be7780141b983dddd1cdb7
jdColonia <JuanDavidColoniaAldana@gmail.com>
2024-11-07 10:27:07 -0500

    chore: add configuration project

---
+++ src/main/java/com/shop_master_backend/ShopMasterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shop_master_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMasterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMasterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop_master_backend/ShopMasterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop_master_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMasterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
