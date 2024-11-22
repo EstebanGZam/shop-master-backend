78a815e9251204a351f7a0bb546c838f0c92d061
Juanma33 <mipus2005@gmail.com>
2024-11-22 00:10:08 -0500

    fix: change in history controller

--- src/main/java/com/shop_master_backend/controller/PurchaseController.java
+++ src/main/java/com/shop_master_backend/controller/PurchaseController.java
@@ -3,8 +3,11 @@ package com.shop_master_backend.controller;
 import com.shop_master_backend.dto.purchase.InvoiceResponseDTO;
 import com.shop_master_backend.dto.purchase.PurchaseRequestDTO;
 import com.shop_master_backend.entity.sql.User;
+import com.shop_master_backend.exception.runtime.UserNotFoundException;
 import com.shop_master_backend.service.interfaces.PurchaseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +27,14 @@ public class PurchaseController {
     }
 
     @GetMapping("/history")
-    public List<InvoiceResponseDTO> getPurchaseHistory(@AuthenticationPrincipal User user) {
-        return purchaseService.getPurchaseHistory(user.getUsername());
+    public ResponseEntity<List<InvoiceResponseDTO>> getPurchaseHistory(@AuthenticationPrincipal User user) {
+        try {
+            List<InvoiceResponseDTO> response = purchaseService.getPurchaseHistory(user.getUsername());
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
